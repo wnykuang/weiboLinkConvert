c3e9381fdb1442ee458ab35eca3d4b26cef8ca9e
wnykuang <kuangwinnie@gmail.com>
2023-03-26 14:58:27 -0700

    feat: Add right-click context menu and copy-to-clipboard functionality
    Add a right-click context menu to allow users to filter out parts of Reddit URLs and copy the filtered URL to the clipboard. This commit adds the necessary `contextMenus` permission and implements the logic to filter out unwanted parts of the URL and copy the filtered URL to the clipboard. The extension now provides a more streamlined user experience and improved functionality.

---
+++ background.js
@@ -0,0 +1,35 @@
+chrome.contextMenus.create({
+  id: "my-context-menu",
+  title: "rightClickMenu",
+  contexts: ["selection"],
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === "my-context-menu") {
+    const filterOutLink = filterRedditLink(info)
+    copyToClipboard(filterOutLink)
+  }
+});
+
+const filterRedditLink = info =>{
+  const linkPrefix = "https://www."
+  const fullLink = info.linkUrl.replace(linkPrefix, "")
+
+  fullLinkParts = fullLink.split('/')
+
+  const combineFilterLink = fullLinkParts.slice(0,5).join("/")
+  return combineFilterLink 
+}
+
+const copyToClipboard = str => {
+  console.log("copyToClipboard");
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
